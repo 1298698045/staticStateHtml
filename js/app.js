12376d958ce2c34dfc82a165b01b1bd574d1b4d1
zd <1298698045@qq.com>
2021-11-12 20:57:39 +0800

    提交代码

--- js/app.js
+++ js/app.js
@@ -6,7 +6,103 @@ var mixin = {
             selectMenu: '',
             isGotop:false,
             qrCodeIdx: 0,
-            isTop:true // 是否在页面顶部
+            isTop:true, // 是否在页面顶部
+            tabs:[
+                {
+                    name: '医院概况',
+                    children:[
+                        {
+                            name: '医院简介'
+                        },
+                        {
+                            name: '机构证件'
+                        },
+                        {
+                            name: '医院文化'
+                        },
+                        {
+                            name: '医院分布图'
+                        },
+                        {
+                            name: '交通指南'
+                        },
+                        {
+                            name: '院领导'
+                        }
+                    ]
+                },
+                {
+                    name: '医院新闻',
+                    children:[
+                        {
+                            name: '医院简介'
+                        },
+                        {
+                            name: '机构证件'
+                        },
+                        {
+                            name: '医院文化'
+                        },
+                        {
+                            name: '医院分布图'
+                        },
+                        {
+                            name: '交通指南'
+                        },
+                        {
+                            name: '院领导'
+                        }
+                    ]
+                },
+                {
+                    name: '院务公开'
+                },
+                {
+                    name: '党建之窗'
+                },
+                {
+                    name: '就医指南'
+                },
+                {
+                    name: '专家推荐',
+                    children:[
+                        {
+                            name: '医院简介'
+                        },
+                        {
+                            name: '机构证件'
+                        },
+                        {
+                            name: '医院文化'
+                        },
+                        {
+                            name: '医院分布图'
+                        },
+                        {
+                            name: '交通指南'
+                        },
+                        {
+                            name: '院领导'
+                        }
+                    ]
+                },
+                {
+                    name: '科研教学'
+                },
+                {
+                    name: '护理园地'
+                },
+                {
+                    name: '医务专栏'
+                },
+                {
+                    name: '健康科普'
+                },
+                {
+                    name: '科室介绍'
+                }
+            ],
+            isFixed: false
         }
     },
     computed: {
@@ -14,8 +110,9 @@ var mixin = {
             return (this.selectMenu || this.typePath)
         }
     },
-    mounted() {
-        window.addEventListener('scroll',this.handleIsScroll,true)
+    mounted:function() {
+        // window.addEventListener('scroll',this.handleIsScroll,true)
+        window.addEventListener('scroll',this.handleSroll)
     },
     methods: {
         push: function(path) {
@@ -33,17 +130,17 @@ var mixin = {
         onMenuMouseLeave: function() {
             this.selectMenu = ''
         },
-        handleTabQrCode(idx){
+        handleTabQrCode:function(idx){
             this.qrCodeIdx = idx;
         },
-        handleIsScroll() {
+        handleIsScroll:function() {
             let scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
             scrolltop > 500 ? (this.isGotop = true) : (this.isGotop = false);
         },
-        toTop() {
+        toTop:function() {
             let top = document.documentElement.scrollTop || document.body.scrollTop;
             // 实现滚动效果 
-            const timeTop = setInterval(() => {
+            const timeTop = setInterval(function() {
                 document.body.scrollTop = document.documentElement.scrollTop = top -= 50;
                 if (top <= 0) {
                     this.isTop = true;
@@ -53,67 +150,83 @@ var mixin = {
                 }
             }, 10);
         },
-        handleOpen(){
+        handleOpen:function(){
             this.$refs.childModal.dialogVisible = true;
-        }
+        },
+        handleSroll:function(e){
+            let top = document.documentElement.scrollTop || document.body.scrollTop;
+            if(top>=140){
+                this.isFixed = true;
+            }else {
+                this.isFixed = false;
+            }
+            
+        },
+        handleMouseOver:function(e,index){
+            $('.wrapper .header .header_nav .nav .column').find('.content').removeClass('active');
+            $('.wrapper .header .header_nav .nav .column').eq(index).find('.content').addClass('active');
+        },
+        handleMouseLeave:function(e){
+            $('.wrapper .header .header_nav .nav .column').find('.content').removeClass('active');
+        },
     }
 }
 
-const Modal = 
-Vue.extend({
-    template: `
-        <el-dialog
-        title="试用申请咨询"
-        :visible.sync="dialogVisible"
-        width="440px"
-        :before-close="handleClose">
-        <div class="modalContent">
-            <div class="desc">完善以下信息您可以免费体验我们的产品功能，我们的顾问会在 1个工作日内与您取得联系</div>
-            <el-form ref="form" :rules="rules" :model="modalForm" label-width="80px">
-                <el-form-item required class="form_item" size="mini" label="单位名称" prop="unitName">
-                    <el-input v-model="modalForm.unitName"></el-input>
-                </el-form-item>
-                <el-form-item required class="form_item" size="mini" label="姓名" prop="name">
-                    <el-input v-model="modalForm.name"></el-input>
-                </el-form-item>
-                <el-form-item required class="form_item" size="mini" label="手机号" prop="phone">
-                    <el-input v-model="modalForm.phone"></el-input>
-                </el-form-item>
-            </el-form>
-        </div>
-        <span slot="footer" class="dialog-footer">
-            <el-button class="submitBtn" type="primary" @click="dialogVisible = false">提交申请</el-button>
-        </span>
-    </el-dialog>
-    `,
-    data(){
-        return {
-            dialogVisible:false,
-            modalForm:{
-                unitName: '',
-                name: '',
-                phone: ''
-            },
-            rules:{
-                unitName:[
-                    { required: true, message: '请输入单位名称', trigger: 'blur' }
-                ],
-                name:[
-                    { required: true, message: '请输入姓名', trigger: 'blur' }
-                ],
-                phone:[
-                    { required: true, message: '请输入电话', trigger: 'blur' }
-                ]
-            }
-        }
-    },
-    methods:{
-        handleClose(){
-            this.dialogVisible = false;
-        },
-        // 弹框
-        handleClose(){
-            this.dialogVisible = false;
-        }
-    }
-})
+// const Modal = 
+// Vue.extend({
+//     template: `
+//         <el-dialog
+//         title="试用申请咨询"
+//         :visible.sync="dialogVisible"
+//         width="440px"
+//         :before-close="handleClose">
+//         <div class="modalContent">
+//             <div class="desc">完善以下信息您可以免费体验我们的产品功能，我们的顾问会在 1个工作日内与您取得联系</div>
+//             <el-form ref="form" :rules="rules" :model="modalForm" label-width="80px">
+//                 <el-form-item required class="form_item" size="mini" label="单位名称" prop="unitName">
+//                     <el-input v-model="modalForm.unitName"></el-input>
+//                 </el-form-item>
+//                 <el-form-item required class="form_item" size="mini" label="姓名" prop="name">
+//                     <el-input v-model="modalForm.name"></el-input>
+//                 </el-form-item>
+//                 <el-form-item required class="form_item" size="mini" label="手机号" prop="phone">
+//                     <el-input v-model="modalForm.phone"></el-input>
+//                 </el-form-item>
+//             </el-form>
+//         </div>
+//         <span slot="footer" class="dialog-footer">
+//             <el-button class="submitBtn" type="primary" @click="dialogVisible = false">提交申请</el-button>
+//         </span>
+//     </el-dialog>
+//     `,
+//     data(){
+//         return {
+//             dialogVisible:false,
+//             modalForm:{
+//                 unitName: '',
+//                 name: '',
+//                 phone: ''
+//             },
+//             rules:{
+//                 unitName:[
+//                     { required: true, message: '请输入单位名称', trigger: 'blur' }
+//                 ],
+//                 name:[
+//                     { required: true, message: '请输入姓名', trigger: 'blur' }
+//                 ],
+//                 phone:[
+//                     { required: true, message: '请输入电话', trigger: 'blur' }
+//                 ]
+//             }
+//         }
+//     },
+//     methods:{
+//         handleClose(){
+//             this.dialogVisible = false;
+//         },
+//         // 弹框
+//         handleClose(){
+//             this.dialogVisible = false;
+//         }
+//     }
+// })
---
+++ js/head.js
@@ -0,0 +1,119 @@
+var headerStr = '<div class="header">'+
+'    <div class="header_top">'+
+'        <div class="header_bd fixedWidth clearfix">'+
+'            <div class="logo fl">'+
+'                <img src="/images/logo.png" alt="">'+
+'            </div>'+
+'            <div class="search fl">'+
+'               <div class="search_box clearfix">'+
+'                   <div class="l_inp fl">'+
+'                       <input type="text" placeholder="请您输入关键词">'+
+'                   </div>'+
+'                   <div class="r_btn fr">'+
+'                       查询'+
+'                   </div>'+
+'               </div>'+
+'            </div>'+
+'            <div class="optionsBtn fr">'+
+'                <p class="btn">'+
+'                    <a class="" href="#">在线院刊</a>'+
+'                </p>'+
+'                <p class="btn">'+
+'                    <a class="" href="#">医保政策</a>'+
+'                </p>'+
+'                <p class="btn">'+
+'                    <a class="" href="#">客户服务</a>'+
+'                </p>'+
+'            </div>'+
+'        </div>'+
+'    </div>'+
+'   <div class="header_nav" :class="{\'active\':isFixed}" ref="headerNav">'+
+'       <ul class="nav fixedWidth">'+
+'           <li class="column" v-for="(item,index) in tabs" @mouseover="function(e){handleMouseOver(e,index)}" @mouseleave="handleMouseLeave">'+
+'               <a href="#">{{item.name}}</a>'+
+'               <div class="content" v-if="item.children">'+
+'                   <div class="menu_wrap">'+
+'                       <div class="menu" v-for="row in item.children">'+
+'                           <a href="#">{{row.name}}</a>'+
+'                       </div>'+
+'                   </div>'+
+'               </div>'+
+'           </li>'+
+'       </ul>'+
+'   </div>'+
+'</div>';
+$('.header_view').append(headerStr);
+var footerStr = '<div class="footer">'+
+'             <div class="footer_top fixedWidth clearfix">'+
+'                 <div class="top_l fl">'+
+'                     <ul class="info">'+
+'                        <li class="dz">通讯地址：福建省福州市仓山区上藤路47号</li>'+
+'                        <li class="yb">   邮编：350007</li>'+
+'                        <li class="dh">'+
+'                            <a href="#">来院导航</a>'+
+'                        </li>'+
+'                     </ul>'+
+'                     <p class="phone">'+
+'                        电话总机：0591-22169222'+
+'                     </p>'+
+'                 </div>'+
+'                 <div class="top_r fr">'+
+'                    邮箱：<a href="#">fzshier@126.com</a>'+
+'                 </div>'+
+'             </div>'+
+'             <div class="footer_center">'+
+'                 <div class="cen_view fixedWidth clearfix">'+
+'                     <div class="cen_l fl clearfix">'+
+'                         <div class="logo_img fl">'+
+'                             <img src="/images/logo_2.png" alt="">'+
+'                         </div>'+
+'                         <div class="hz_img fl">'+
+'                             <img src="/images/qualification.png" alt="">'+
+'                         </div>'+
+'                     </div>'+
+'                     <div class="cen_r fr">'+
+'                         <ul class="box_wrap">'+
+'                            <li class="box">'+
+'                                <div class="code_img"></div>'+
+'                                <p class="name">官方微博</p>'+
+'                            </li>'+
+'                            <li class="box">'+
+'                                <div class="code_img"></div>'+
+'                                <p class="name">官方微信订阅号</p>'+
+'                            </li>'+
+'                            <li class="box">'+
+'                                <div class="code_img"></div>'+
+'                                <p class="name">官方微信服务号</p>'+
+'                            </li>'+
+'                            <li class="box">'+
+'                                <div class="code_img"></div>'+
+'                                <p class="name">官方抖音</p>'+
+'                            </li>'+
+'                         </ul>'+
+'                     </div>'+
+'                 </div>'+
+'             </div>'+
+'             <div class="footer_foot fixedWidth clearfix">'+
+'                 <div class="foot_l fl">'+
+'                     <ul class="route">'+
+'                        <li class="badge">'+
+'                            <img src="/images/badge.png" alt="">'+
+'                           <a href="#">闽公网安备 35010402350152号</a>'+
+'                       </li>'+
+'                       <li class="beia">'+
+'                            <a href="#">闽ICP备13015393号</a>'+
+'                        </li>'+
+'                     </ul>'+
+'                 </div>'+
+'                 <div class="foot_r fr">'+
+'                     <ul>'+
+'                         <li class="text">'+
+'                            版权所有：2021 福建省福州市第二医院 All right reserved'+
+'                         </li>'+
+'                         '+
+'                        <li>网站建设：<a href="#">北京凤凰世纪科技有限公司</a></li>'+
+'                     </ul>'+
+'                 </div>'+
+'             </div>'+
+'         </div>';
+$('.footer_view').append(footerStr);
